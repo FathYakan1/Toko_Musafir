f59ebed4722e80b841820d9e02648e004e7fd625
Fath Yakan <Fathyakan2@gmail.com>
2025-02-23 21:49:31 -0800

    First commit

---
+++ resources/views/layouts/backend/data/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      Copyright &copy; {{ Date('Y') }} <div class="bullet"></div> Admin Template By <a href="https://nauval.in/">Musafir Store</a> Developed Apps By <a href="https://instagram.com/amerta.xx">Fath Yakan</a></p>
+    </div>
+    <div class="footer-right">
+      2.3.0
+    </div>
+  </footer>
